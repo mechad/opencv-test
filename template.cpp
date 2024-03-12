b070094b8d09f9986612ee14ad3eeee4d3e480de
chad <linczone@1163.com>
2024-03-12 17:08:29 +0800

    整理格式

--- template.cpp
+++ template.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
        if (mask.rows != templ.rows || mask.cols != templ.cols) {
          std::cout << "mask.size = " << mask.rows << "x" << mask.cols << std::endl;
          std::cout << "templ.size = " << templ.rows << "x" << templ.cols << std::endl;
-	 return 1;
+	       return 1;
        }
        if (templ.channels() > 1) cv::cvtColor(templ, templ, cv::COLOR_RGB2GRAY); //将原图转换为灰度图像
     }
@@ -39,22 +39,22 @@ int main(int argc, char** argv) {
     cv::Mat result;
 
     while (cap.read(frame)) {
-	auto start = std::chrono::high_resolution_clock::now();
-	// 使用灰度图进行模板匹配时，耗时0.07 秒，同样的数据采用彩图进行匹配需要花费0.21s,相差了3倍
-        cv::cvtColor(frame, frame, cv::COLOR_RGB2GRAY); //将原图转换为灰度图像
-	// 匹配的准确度，全彩图匹配成功率略高
-	if (argc == 4) {
-	    // 使用mask 时，图像格式必须一致，也就是都要灰度图
-	    // 只有 TM_CCORR_NORMED 模式下使用 mask 掩码模板成功
-	    // 有mask的模式很难区别是否检测到，结果变化太小
-	    cv::matchTemplate(frame, templ, result, cv::TM_CCORR_NORMED, mask);
-	} else {
-	    cv::matchTemplate(frame, templ, result, cv::TM_CCOEFF_NORMED);
-	}
-	auto end = std::chrono::high_resolution_clock::now();
-	// 计算执行时间
-	std::chrono::duration<double> duration = end - start;
-	std::cout << "函数执行时间: " << duration.count() << " 秒" << std::endl;
+        auto start = std::chrono::high_resolution_clock::now();
+        // 使用灰度图进行模板匹配时，耗时0.07 秒，同样的数据采用彩图进行匹配需要花费0.21s,相差了3倍
+              cv::cvtColor(frame, frame, cv::COLOR_RGB2GRAY); //将原图转换为灰度图像
+        // 匹配的准确度，全彩图匹配成功率略高
+        if (argc == 4) {
+            // 使用mask 时，图像格式必须一致，也就是都要灰度图
+            // 只有 TM_CCORR_NORMED 模式下使用 mask 掩码模板成功
+            // 有mask的模式很难区别是否检测到，结果变化太小
+            cv::matchTemplate(frame, templ, result, cv::TM_CCORR_NORMED, mask);
+        } else {
+            cv::matchTemplate(frame, templ, result, cv::TM_CCOEFF_NORMED);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        // 计算执行时间
+        std::chrono::duration<double> duration = end - start;
+        std::cout << "函数执行时间: " << duration.count() << " 秒" << std::endl;
 
         double minVal, maxVal;
         cv::Point minLoc, maxLoc;
@@ -62,16 +62,16 @@ int main(int argc, char** argv) {
 
         cv::Point matchLoc = maxLoc;
 
-	if (maxVal > 0.4)
-        cv::rectangle(frame, matchLoc, cv::Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), cv::Scalar(0, 255, 0), 2);
+        if (maxVal > 0.4)
+          cv::rectangle(frame, matchLoc, cv::Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), cv::Scalar(0, 255, 0), 2);
 
         // 调整显示图像框大小
         cv::resize(frame, frame, cv::Size(frame.cols/2, frame.rows/2));
         // Convert maxVal to string
         std::stringstream ss;
         ss << "Similarity: (max)" << maxVal;
-	ss << "  (min) " << minVal;
-	std::cout << ss.str() << std::endl;
+	      ss << "  (min) " << minVal;
+	      std::cout << ss.str() << std::endl;
 
         // Print maxVal on the image
         cv::putText(frame, ss.str(), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
